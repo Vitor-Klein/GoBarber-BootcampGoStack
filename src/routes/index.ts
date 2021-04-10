02849c65a173ff98a694354c593875925e8b209f
Vitor Klein <vitorhklein2@gmail.com>
2021-04-10 16:37:10 -0300

    aula sobre model de agendamentos completa

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,16 +1,8 @@
 import { Router } from 'express';
+import appointmentsRouter from './appointments.routes'
 
 const routes = Router();
+routes.use('/appointments', appointmentsRouter);
 
-routes.post('/users', (request, response) => {
-  const { name, email } = request.body;
-
-  const user = {
-    name,
-    email,
-  };
-
-  return response.json(user);
-});
 
 export default routes;
