742fb6a05f8a0783033672d6e3a04202914f8f22
Vitor Klein <vitorhklein2@gmail.com>
2021-04-18 00:57:20 -0300

    change repositories to use typeorm

--- server/src/database/migrations/1618632576451-CreateAppointments.ts
+++ server/src/database/migrations/1618632576451-CreateAppointments.ts
@@ -11,6 +11,7 @@ export default class CreateAppointments1618632576451 implements MigrationInterfa
               type: 'varchar',
               isPrimary: true,
               generationStrategy: 'uuid',
+              default: 'uuid_generate_v4()'
             },
             {
               name: 'provider',
