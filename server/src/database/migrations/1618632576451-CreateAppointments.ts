26885c66f37509964e1b5b03e50dc54b212ffb0f
Vitor Klein <vitorhklein2@gmail.com>
2021-04-27 01:10:40 -0300

    create model relationship

--- server/src/database/migrations/1618632576451-CreateAppointments.ts
+++ server/src/database/migrations/1618632576451-CreateAppointments.ts
@@ -8,7 +8,7 @@ export default class CreateAppointments1618632576451 implements MigrationInterfa
           columns: [
             {
               name: 'id',
-              type: 'varchar',
+              type: 'uuid',
               isPrimary: true,
               generationStrategy: 'uuid',
               default: 'uuid_generate_v4()'
