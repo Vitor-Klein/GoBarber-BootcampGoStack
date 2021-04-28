1943b4e973d8415dc0a367924a124ace1e839f59
Vitor Klein <vitorhklein2@gmail.com>
2021-04-28 01:49:35 -0300

    generate JWT token

--- server/src/services/AuthenticateUserService.ts
+++ server/src/services/AuthenticateUserService.ts
@@ -1,5 +1,6 @@
 import { getRepository } from 'typeorm'
 import { compare } from 'bcryptjs'
+import { sign } from 'jsonwebtoken'
 
 import User from '../models/User'
 
@@ -10,6 +11,7 @@ interface Request {
 
 interface Response {
   user: User;
+  token: string;
 }
 
 class AuthenticateUserService {
@@ -28,8 +30,14 @@ class AuthenticateUserService {
       throw new Error('incorrect email/password combination.');
     }
 
+    const token = sign({  }, '7c11a2130194e87ff7afac3102d5d2a4', {
+      subject: user.id,
+      expiresIn: '1d',
+    });
+
     return {
       user,
+      token,
     }
   }
 }
