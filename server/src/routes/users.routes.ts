bccdcc1aeb6a93d21dc2f9327bcb68dd4f8977dd
Vitor Klein <vitorhklein2@gmail.com>
2021-05-04 00:54:14 -0300

    update avatar complete

--- server/src/routes/users.routes.ts
+++ server/src/routes/users.routes.ts
@@ -3,6 +3,7 @@ import multer from 'multer'
 import uploadConfig from '../config/upload'
 
 import CreateUserService from '../services/CreateUserService'
+import UpdateUserAvatarService from '../services/UpdateUserAvatarService'
 
 import ensureAuthenticated from '../middlewares/ensureAuthenticated'
 
@@ -40,9 +41,26 @@ usersRouter.patch(
   ensureAuthenticated,
   upload.single('avatar'),
   async(request, response) => {
-    console.log(request.file);
+    try{
+      const updateUserAvatar = new UpdateUserAvatarService()
 
-    return response.json({ ok: true })
+      const user = await updateUserAvatar.execute({
+        user_id: request.user.id,
+        avatarFilename: request.file.filename,
+      })
+
+      const userWithoutPassword = {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        created_at: user.created_at,
+        updated_at: user.updated_at,
+      };
+
+      return response.json(userWithoutPassword);
+    }catch (err){
+      return response.status(400).json({ error: err.message });
+    }
   },
 );
 
