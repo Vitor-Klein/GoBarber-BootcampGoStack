7bf9b37dc259055ff1eb892cc81a34b8d3405a41
Vitor Klein <vitorhklein2@gmail.com>
2021-05-13 00:49:48 -0300

    utilizing unform

--- gobarber-web/src/pages/SignUp/index.tsx
+++ gobarber-web/src/pages/SignUp/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { FiArrowLeft, FiMail, FiUser, FiLock } from 'react-icons/fi';
+import { Form } from '@unform/web'
 
 import logoImg from '../../assets/logo.svg';
 
@@ -9,6 +10,10 @@ import Input from "../../components/Input";
 import Button from "../../components/Button";
 
 const SignUp: React.FC = () => {
+  function handleSubmit(data: object):void {
+    console.log(data)
+  }
+
   return (
     <Container>
       <Background />
@@ -16,7 +21,7 @@ const SignUp: React.FC = () => {
       <Content>
         <img src={logoImg} alt="GoBarber" />
 
-        <form>
+        <Form onSubmit={handleSubmit}>
           <h1>Faça seu cadastro</h1>
 
           <Input name="name" icon={FiUser} placeholder="Nome" />
@@ -31,7 +36,7 @@ const SignUp: React.FC = () => {
           
           <Button type="submit">Cadastrar</Button>
 
-        </form>
+        </Form>
 
         <a href="signup">
           <FiArrowLeft />
