ca09ff9b40789d7033a02d8a0e5557f1b945288a
Vitor Klein <vitorhklein2@gmail.com>
2021-05-19 01:19:29 -0300

    validate schema

--- gobarber-web/src/pages/SignUp/index.tsx
+++ gobarber-web/src/pages/SignUp/index.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { FiArrowLeft, FiMail, FiUser, FiLock } from 'react-icons/fi';
 import { Form } from '@unform/web'
+import * as Yup from 'yup'
 
 import logoImg from '../../assets/logo.svg';
 
@@ -8,11 +9,26 @@ import { Container, Content, Background } from './styles';
 
 import Input from "../../components/Input";
 import Button from "../../components/Button";
+import schema from 'yup/lib/schema';
 
 const SignUp: React.FC = () => {
-  function handleSubmit(data: object):void {
-    console.log(data)
-  }
+  const handleSubmit = useCallback(async(data: object) => {
+    try{
+      const schema = Yup.object().shape({
+        name: Yup.string().required('Nome obrigatório'),
+        email: Yup.string()
+          .required('E-mail obrigatório')
+          .email('Digte um email valido'),
+        password: Yup.string().min(6, 'No minimo 6 dígitos'),
+      });
+
+      await schema.validate(data, {
+        abortEarly: false
+      })
+    }catch(err){
+      console.log(err)
+    }
+  }, [])
 
   return (
     <Container>
