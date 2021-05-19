8b368abab4faf3d6e55be4318fc97c9754a58595
Vitor Klein <vitorhklein2@gmail.com>
2021-05-19 00:59:54 -0300

    input usability

--- gobarber-web/src/components/Input/styles.ts
+++ gobarber-web/src/components/Input/styles.ts
@@ -1,17 +1,33 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
-export const Container = styled.div`
+interface ContainerProps {
+  isFocused: boolean;
+  isFilled: boolean;
+}
+
+export const Container = styled.div<ContainerProps>`
   background: #232129;
   border-radius: 10px;
-  border: 2px solid #232129;
   padding: 16px;
+
+  border: 2px solid #232129;
   color: #666360;
+
   width: 100%;
   display: flex;
   align-items: center;
   & + div {
     margin-top: 8px;
   }
+
+  ${props => props.isFocused && css `
+    border: 2px solid #ff9000;
+    color: #ff9000;
+  `}
+
+  ${props => props.isFilled && css `
+    color: #ff9000;
+  `}
   input {
     flex: 1;
     background: transparent;
