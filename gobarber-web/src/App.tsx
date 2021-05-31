7af0539d0fba61ed288c24bc91cdd5432f1fde93
Vitor Klein <vitorhklein2@gmail.com>
2021-05-31 01:57:54 -0300

    auth to context

--- gobarber-web/src/App.tsx
+++ gobarber-web/src/App.tsx
@@ -4,14 +4,14 @@ import GlobalStyle from './styles/global';
 import SignIn from './pages/SignIn'
 // import SignUp from './pages/SignUp'
 
-import AuthContext from './context/AuthContext'
+import { AuthProvider } from './context/AuthContext'
 
 const App: React.FC = () => {
   return (
     <>
-      <AuthContext.Provider value={{ name: 'Vitor' }}>
+      <AuthProvider>
         <SignIn />
-      </AuthContext.Provider>
+      </AuthProvider>
 
       <GlobalStyle />
     </>
--- gobarber-web/src/pages/SignIn/index.tsx
+++ gobarber-web/src/pages/SignIn/index.tsx
@@ -4,7 +4,7 @@ import { FormHandles } from '@unform/core';
 import { Form } from '@unform/web';
 import * as Yup from 'yup';
 
-import AuthContext from '../../context/AuthContext';
+import { AuthContext } from '../../context/AuthContext';
 import getValidationErrors from '../../utils/getValidationErrors';
 
 import logoImg from '../../assets/logo.svg';
@@ -14,14 +14,17 @@ import { Container, Content, Background } from './styles';
 import Input from "../../components/Input";
 import Button from "../../components/Button";
 
+interface SignInFormData {
+  email: string;
+  password: string;
+}
+
 const SignIn: React.FC = () => {
   const formRef = useRef<FormHandles>(null);
 
-  const { name } = useContext(AuthContext);
+  const { signIn } = useContext(AuthContext);
 
-  console.log(name)
-
-  const handleSubmit = useCallback(async(data: object) => {
+  const handleSubmit = useCallback(async(data: SignInFormData) => {
     try{
       formRef.current?.setErrors({});
 
@@ -35,12 +38,17 @@ const SignIn: React.FC = () => {
       await schema.validate(data, {
         abortEarly: false
       });
+
+      signIn({
+        email: data.email,
+        password: data.password,
+      });
     }catch(err){
       const errors = getValidationErrors(err)
 
       formRef.current?.setErrors(errors);
     }
-  }, [])
+  }, [signIn]);
 
   return (
     <Container>
