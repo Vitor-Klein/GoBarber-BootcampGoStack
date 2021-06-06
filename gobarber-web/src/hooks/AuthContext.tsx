71c7236bd09c14a7bea6b4990115cf645e04fa2a
Vitor Klein <vitorhklein2@gmail.com>
2021-06-06 01:53:25 -0300

    logout with aplication

--- gobarber-web/src/hooks/AuthContext.tsx
+++ gobarber-web/src/hooks/AuthContext.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useCallback, useState } from 'react';
+import React, { createContext, useCallback, useState, useContext } from 'react';
 import api from '../services/api';
 
 interface AuthState {
@@ -14,6 +14,7 @@ interface SignInCredentials {
 interface AuthContextData {
   user: object;
   signIn(credentials: SignInCredentials): Promise<void>;
+  signOut(): void;
 }
 
 const AuthContext = createContext<AuthContextData>({} as AuthContextData);
@@ -21,7 +22,7 @@ const AuthContext = createContext<AuthContextData>({} as AuthContextData);
 const AuthProvider: React.FC = ({ children }) => {
   const [data, setData] = useState<AuthState>(() => {
     const token = localStorage.getItem('@Gobarber:token');
-    const user = localStorage.getItem('@Gobarber:token');
+    const user = localStorage.getItem('@Gobarber:user');
 
     if(token && user) {
       return { token, user: JSON.parse(user) }
@@ -39,16 +40,34 @@ const AuthProvider: React.FC = ({ children }) => {
     const { token, user } = response.data;
 
     localStorage.setItem('@Gobarber:token', token);
-    localStorage.setItem('@Gobarber:token', JSON.stringify(user));
+    localStorage.setItem('@Gobarber:user', JSON.stringify(user));
 
     setData({ token, user });
   }, []);
 
+  const signOut = useCallback(() => {
+    localStorage.removeItem('@Gobarber:token');
+    localStorage.removeItem('@Gobarber:user');
+
+    setData({} as AuthState);
+
+  }, [])
+
   return (
-    <AuthContext.Provider value={{ user: data.user, signIn }}>
+    <AuthContext.Provider value={{ user: data.user, signIn, signOut }}>
       {children}
     </AuthContext.Provider>
   );
 };
 
-export { AuthContext, AuthProvider };
+function useAuth(): AuthContextData {
+  const context = useContext(AuthContext)
+
+  if(!context){
+    throw new Error('userAuth must be used within an AuthProvider');
+  }
+
+  return context;
+}
+
+export { AuthProvider, useAuth };
